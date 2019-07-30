2d9bbe17e4dd21e5390fc15b9edc4542bdb9c00e
Okeyobia <okey.obia@gmail.com>
2019-07-29 23:14:05 -0400

    initial commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Put, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private taskService: TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ) {
+        return this.taskService.createTask(title, description);
+    }
+}
